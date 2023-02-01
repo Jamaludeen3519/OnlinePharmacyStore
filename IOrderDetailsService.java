5c814a0eeb883cf166e032a50f27422a3a57feb7
AkurathiVeda <122590124+AkurathiVeda@users.noreply.github.com>
2023-02-01 17:35:41 +0530

    Add files via upload

---
+++ IOrderDetailsService.java
@@ -0,0 +1,18 @@
+package com.cg.onlinepharmacystore.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.cg.onlinepharmacystore.entity.OrderDetails;
+import com.cg.onlinepharmacystore.exception.ResourceNotFoundException;
+
+public interface IOrderDetailsService {
+	
+	public OrderDetails orderDetails(String orderId,OrderDetails orderDetail) throws ResourceNotFoundException;
+	public List<OrderDetails> getAllOrderDetailsByOrderId(String orderId);
+	public List<OrderDetails> getOrderDetailsByAdminId(Long adminId);
+	   public ResponseEntity<OrderDetails> updateAdmin(Long orderDetailId,
+		         OrderDetails orderDetail) throws ResourceNotFoundException ;
+
+}
