82ad696673fbee8a25bacaf1b92eaeeb35ab4dff
Pravin Prasad <54862010+Pravin-Prasad@users.noreply.github.com>
2023-02-01 16:14:59 +0530

    Add files via upload

---
+++ IProductService.java
@@ -0,0 +1,20 @@
+package com.cg.onlinepharmacystore.service;
+
+import java.util.*;
+
+import org.springframework.http.ResponseEntity;
+import com.cg.onlinepharmacystore.entity.Products;
+import com.cg.onlinepharmacystore.exception.ResourceNotFoundException;
+
+
+
+
+public interface IProductService {
+
+
+	public Products addProduct(Products product);
+	public List<Products> getListAllProducts();
+	public Products getProductById(Long productId) throws ResourceNotFoundException;
+	public Products updateProduct(Products pro,Long productId) throws ResourceNotFoundException;
+//	public Products findByProductName(String product_Name) throws ResourceNotFoundException;
+}
